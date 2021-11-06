0d11872fdc133ad53381d4043b2d287199ef9863
LeonardoBujan <tatobujan86@gmail.com>
2021-11-05 21:38:36 -0300

    agregando archivo procfile y configurando puerto dinamico

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require("path");
 
 //localhost:3000
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("servido encendido");
 });
 
